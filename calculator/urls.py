9ddfcde55b6dd75239a4ac93f0738677294202b0
amirbagaon <amirbaga2004@gmail.com>
2021-02-23 03:04:04 +0200

    added first page

--- calculator/urls.py
+++ calculator/urls.py
@@ -20,4 +20,5 @@ from main import views as main_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_views.home, name='home')
+    path('first', main_views.home, name='first')
 ]
